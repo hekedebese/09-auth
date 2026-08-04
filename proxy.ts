6511b25f623da43c2edd568464a575160b60f3ba
xenoterror <anohinnikita537@gmail.com>
2026-08-05 00:26:03 +0300

    10 redirect fix

--- proxy.ts
+++ proxy.ts
@@ -39,7 +39,7 @@ export default async function proxy(request: NextRequest) {
         }
 
         if (isPublicRoute) {
-          return NextResponse.redirect(new URL("/profile", request.url), {
+          return NextResponse.redirect(new URL("/", request.url), {
             headers: {
               Cookie: cookieStore.toString(),
             },
